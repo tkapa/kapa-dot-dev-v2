0f4f73f67173e4ebe92db00c3c053c16ce88c7bb
Tylah Kapa <tylahkapa@gmail.com>
2023-12-13 22:20:14 +1000

    Update with Gone Fish

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,10 +1,9 @@
 const projectsData = [
   {
     title: 'Gone Fish',
-    description: `Ever wanted to make a game? It's insanely difficult! But you can do it.
-     I created this simple fishing game with a group of people for a game jam in order to learn Rust! Find out more below`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/gone-fish',
+    description: `A simple fishing game created in Rust for GitHub Game Off 2023!`,
+    imgSrc: '/static/images/gone-fish-image.png',
+    href: 'https://kapa.itch.io/gone-fish',
   },
 ]
 
