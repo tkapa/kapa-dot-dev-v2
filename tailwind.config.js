e0bb2e49e4afa39fa2b44c206136f0d432d0bb88
Tylah Kapa <tylahkapa@gmail.com>
2024-02-07 11:40:48 +1000

    Use Geist

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
         14: '3.5rem',
       },
       fontFamily: {
-        sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
+        sans: ['geist', ...fontFamily.sans],
       },
       colors: {
         primary: colors.pink,
