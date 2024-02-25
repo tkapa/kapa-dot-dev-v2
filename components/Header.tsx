9f2201e707064a33829d3485c1b8b45d6429ee9e
Tylah Kapa <tylahkapa@gmail.com>
2024-02-26 09:59:24 +1000

    Fix build

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,32 +8,32 @@ import ThemeImage from './ThemeImage'
 
 const Header = () => {
   return (
-    <header className='flex items-center justify-between'>
+    <header className="flex items-center justify-between">
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
-
-          <div className='flex items-center justify-between'>1
+          <div className="flex items-center justify-between">
             {typeof siteMetadata.headerTitle === 'string' ? (
-                <ThemeImage 
-                srcLight='/static/images/branding/full-logo-light.svg'
-                srcDark='/static/images/branding/full-logo-dark.svg' 
-                alt='' 
-                width={150} 
-                height={150} />
+              <ThemeImage
+                srcLight="/static/images/branding/full-logo-light.svg"
+                srcDark="/static/images/branding/full-logo-dark.svg"
+                alt=""
+                width={150}
+                height={150}
+              />
             ) : (
               siteMetadata.headerTitle
             )}
           </div>
         </Link>
       </div>
-      <div className='flex items-center leading-5 space-x-4 sm:space-x-6'>
+      <div className="flex items-center leading-5 space-x-4 sm:space-x-6">
         {headerNavLinks
           .filter((link) => link.href !== '/')
           .map((link) => (
             <Link
               key={link.title}
               href={link.href}
-              className='hidden sm:block font-medium text-gray-900 dark:text-gray-100'
+              className="hidden sm:block font-medium text-gray-900 dark:text-gray-100"
             >
               {link.title}
             </Link>
