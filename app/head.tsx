ea7c104ecab082f5603cc0c92abe6f86fc2e0ef3
Tylah Kapa <tylahkapa@gmail.com>
2024-05-31 14:39:26 +0200

    Remove 32x32 Branding

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,6 @@ export default function Head() {
   return (
     <>
       <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/kapa-red-76x76.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/kapa-red-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/kapa-red-16x16.png" />
       <link rel="manifest" href="/static/favicons/site.webmanifest" />
       <meta name="msapplication-TileColor" content="#000000" />
